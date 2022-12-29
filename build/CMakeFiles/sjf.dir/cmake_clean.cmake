1e22ec14e3dd52a94885fa9a2a4078a6182c21c3
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2022-12-29 03:09:43 +0300

    sjf_algoritm

---
+++ build/CMakeFiles/sjf.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sjf.dir/main.cpp.o"
+  "CMakeFiles/sjf.dir/main.cpp.o.d"
+  "CMakeFiles/sjf.dir/process.cpp.o"
+  "CMakeFiles/sjf.dir/process.cpp.o.d"
+  "CMakeFiles/sjf.dir/sjf.cpp.o"
+  "CMakeFiles/sjf.dir/sjf.cpp.o.d"
+  "sjf"
+  "sjf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sjf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
